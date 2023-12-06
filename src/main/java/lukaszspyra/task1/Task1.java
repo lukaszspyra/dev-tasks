fba3a04ba846af4943976fd28b6d4e2c1d1aedad
Lukasz Spyra <lukasz.spyrka@gmail.com>
2023-12-06 13:17:11 +0100

    Add User interaction with input data for Task1
    Replaced generated String with batch processing using Map of integers lists, with key representing batch number.

--- src/main/java/lukaszspyra/task1/Task1.java
+++ src/main/java/lukaszspyra/task1/Task1.java
@@ -1,32 +1,53 @@
 package lukaszspyra.task1;
 
-import java.io.PrintStream;
-import java.util.Arrays;
 import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Task1 {
 
+  private final Console console;
+  private final int batchSize;
+
+  Task1(final int batchSize, final Console console) {
+    this.batchSize = batchSize;
+    this.console = console;
+  }
+
+  void startApp() {
+    console.task1Usage();
+    final List<Integer> input = console.readNumberList();
+    console.printNumbers(batchProcessInput(input));
+    console.printStats(calculateStats(input));
+  }
+
   /**
-   * Used as a field to capture state for output unit test.
+   * Batch processes list of integers. Result is distinct and sorted naturally.
+   *
+   * @param input integers list
+   * @return map with key representing batch number and corresponding lists of batched integers
    */
-  private PrintStream out;
-
-  Task1(PrintStream out) {
-    this.out = out;
-  }
-
-  void processInput(final int[] input) {
-    String stats = calculateStats(input);
-    out.print(Arrays.stream(input).distinct().sorted().mapToObj(String::valueOf).collect(Collectors.joining(" ", "", "\n")));
-    out.print(stats);
-  }
-
-  private String calculateStats(final int[] input) {
-    final long size = input.length;
-    final IntSummaryStatistics statistics = Arrays.stream(input)
+  Map<Integer, List<Integer>> batchProcessInput(final List<Integer> input) {
+    return input.stream()
         .distinct()
         .sorted()
+        .collect(Collectors.groupingBy(i -> i / batchSize));
+  }
+
+  /**
+   * Calculates statistics of given integer list.
+   * Includes: count, distinct count, min, max.
+   *
+   * @param input integers
+   * @return String with stats
+   */
+  String calculateStats(final List<Integer> input) {
+    final long size = input.size();
+    final IntSummaryStatistics statistics = input.stream()
+        .distinct()
+        .sorted()
+        .mapToInt(Integer::intValue)
         .summaryStatistics();
     return "count: " + size + "\n" +
         "distinct: " + statistics.getCount() + "\n" +
--- src/test/java/lukaszspyra/task1/Task1Test.java
+++ src/test/java/lukaszspyra/task1/Task1Test.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit tests for the first task.
@@ -15,20 +18,23 @@ public class Task1Test {
   @Test
   public void outputShallBeDistinctSortedWithMeasurements() {
     //given
-    int[] input = new int[]{1, 10, 20, 20, 2, 5};
+    List<Integer> inputList = Arrays.asList(1, 10, 20, 20, 2, 5);
     String expectedOutput = "1 2 5 10 20\n" +
         "count: 6\n" +
         "distinct: 5\n" +
         "min: 1\n" +
         "max: 20";
     final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    Task1 task1 = new Task1(new PrintStream(outputStream));
+    final Console testConsole = new Console(new Scanner("test"), new PrintStream(outputStream), null);
+    Task1 task1 = new Task1(2, testConsole);
 
     //when
-    task1.processInput(input);
+    testConsole.printNumbers(task1.batchProcessInput(inputList));
+    testConsole.printStats(task1.calculateStats(inputList));
 
     //then
-    assertEquals(expectedOutput, outputStream.toString());
+    final String s = outputStream.toString();
+    assertTrue(s.contains(expectedOutput));
   }
 
 }
