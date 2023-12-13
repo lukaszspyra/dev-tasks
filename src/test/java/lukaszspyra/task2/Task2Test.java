8e7f37cc50825e4c741c11fde0c8ce8653d6872c
Lukasz Spyra <lukasz.spyrka@gmail.com>
2023-12-13 20:00:57 +0100

    Add second test case for Task2

--- src/test/java/lukaszspyra/task2/Task2Test.java
+++ src/test/java/lukaszspyra/task2/Task2Test.java
@@ -1,12 +1,15 @@
 package lukaszspyra.task2;
 
 import lukaszspyra.Console;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,14 +18,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class Task2Test {
 
-  @Test
-  public void shallFindPairsSummingTo13() {
+  @ParameterizedTest
+  @MethodSource("integerListsWithPairSumming13")
+  public void shallFindPairsSummingTo13(List<Integer> input, String expectedOutput) {
     //given
-    List<Integer> input = Arrays.asList(1, 2, 10, 7, 5, 3, 6, 6, 13, 0);
-    String expectedOutput = "0 13\n" +
-        "3 10\n" +
-        "6 7\n" +
-        "6 7";
     final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     Task2 task2 = new Task2(new Console(null, new PrintStream(outputStream), null));
 
@@ -31,6 +30,27 @@ public class Task2Test {
 
     //then
     assertEquals(expectedOutput, outputStream.toString());
+  }
+
+  private static Stream<Arguments> integerListsWithPairSumming13() {
+    return Stream.of(
+        Arguments.of(Arrays.asList(1, 2, 10, 7, 5, 3, 6, 6, 13, 0),
+            "0 13\n" +
+                "3 10\n" +
+                "6 7\n" +
+                "6 7"),
+        Arguments.of(Arrays.asList(1, 2, 10, 7, 5, 3, 6, 7, 13, 0),
+            "0 13\n" +
+                "3 10\n" +
+                "6 7\n" +
+                "6 7"),
+        Arguments.of(Arrays.asList(13, 13, 0, 0),
+            "0 13\n" +
+                "0 13\n" +
+                "0 13\n" +
+                "0 13"
+        )
+    );
   }
 
 }
